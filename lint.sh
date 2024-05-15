b90d5d44ed51af5b6a1c3bd6d590cd0f63d00580
Zach Butler <kj4ezj@pm.me>
2024-05-15 16:01:37 -0400

    Add CI

--- lint.sh
+++ lint.sh
@@ -7,5 +7,11 @@ source ./deps/bin/ee
 printf '\e[1;35m===== Lint archive.sh =====\e[0m\n'
 ee bashate -i E006 archive.sh
 ee shellcheck -e SC1091 -f gcc archive.sh
+# lint CI code
+printf '\e[37m===== Lint CI =====\e[0m\n'
+ee bashate -i E006 .github/workflows/deps.sh
+ee shellcheck -e SC2294 -x -f gcc .github/workflows/deps.sh
+ee bashate -i E006 lint.sh
+ee shellcheck -x -f gcc lint.sh
 
 echo "Done. - ${0##*/}"
